0e720c8edad3c73351a245602848c6732823c596
Navied-dipu <navieddipu08@gmail.com>
2026-05-07 23:53:11 +0600

    first commit

---
+++ Control Flow & Boolean Logic/task1.js
@@ -0,0 +1,18 @@
+
+// ### 1. **Simple Age Gate**
+// **Question:** Write a function that takes a person's age and returns
+// what they are allowed to do.
+// Rules:
+// - Under 13 → "Can watch cartoons only"
+// - 13 to 17 → "Can watch PG-13 movies"
+// - 18 and above → "Can watch all movies"
+
+// **Sample Data:**
+// const ages = [8, 15, 20, 13, 17];
+
+// **Expected Output:**
+// 8  → "Can watch cartoons only"
+// 15 → "Can watch PG-13 movies"
+// 20 → "Can watch all movies"
+// 13 → "Can watch PG-13 movies"
+// 17 → "Can watch PG-13 movies"
